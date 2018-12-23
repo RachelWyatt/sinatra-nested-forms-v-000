9650126952d5eca9adac94d824ed13b9dece5c3c
Rachel Wyatt <rw126@evansville.edu>
2018-12-23 15:11:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,10 +12,12 @@ module FormsLab
     end
 
     post '/pirates' do
-      @pirate = Pirate.new(params[:pirate])
-      params[:pirate][:ships].each do |details|
-        Ship.new(details)
+      @pirate = Pirate.new(params["pirate"]["name"], params["pirate"]["weight"], params["pirate"]["height"])
+
+      params["pirate"]["ships"].each do |data|
+        Ship.new(data["name"], data["type"], data["booty"])
       end
+
       @ships = Ship.all
 
       erb :"pirates/show"

512cef37f51032644904f355b35d02d72fd66783
Rachel Wyatt <rw126@evansville.edu>
2018-12-23 14:25:05 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,16 @@
 class Ship
+  attr_accessor :name, :type, :booty_attributes
+
+  @@all = []
+
+  def initialize(name, type, booty_attributes)
+    @name = name
+    @type = type
+    @booty_attributes = booty_attributes 
+    @@all << self
+  end
+
+  def self.all 
+    @@all 
+  end
 end

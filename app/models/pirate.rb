0a67272c79fd5b16a42e3bec2c3e61eea93d970a
Rachel Wyatt <rw126@evansville.edu>
2018-12-23 14:22:04 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -3,8 +3,13 @@ class Pirate
   @@all = []
 
   def initialize(name, weight, height)
-    @name = name 
-    @weight = weight 
+    @name = name
+    @weight = weight
     @height = height
+    @@all << self
+  end
+
+  def self.all 
+    @@all
   end
 end
